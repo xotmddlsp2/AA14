f57e13e307f4799627569f84655f25e9b2ba35c4
xotmddlsp2 <79964307+xotmddlsp2@users.noreply.github.com>
2021-09-08 17:14:24 +0900

    Add files via upload

---
+++ aa14-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+    console.log("Hello " + what + " !");
+}
+
+hello("aa14");
+hello("xotmddlsp2, 고태승");
---
+++ aa14-rpt02/start/index_aa14.js
@@ -0,0 +1,3 @@
+var myinfo = require('./aa14info');
+myinfo("aa14", "xotmddlsp2", '010-8863-9925');
+myinfo("aa14", "xotmddlsp2", '010-8863-9925');
